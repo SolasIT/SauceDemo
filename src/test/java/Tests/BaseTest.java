0e74cc843e326f7bd959fc224dc302b7fc46c44a
Olga <o.felassan@gmail.com>
2025-07-28 18:42:15 +0300

    fix base test

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -39,6 +39,7 @@ public class BaseTest {
             options.addArguments("--disable-notifications");
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
+            options.addArguments(("--headless"));
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
@@ -57,6 +58,8 @@ public class BaseTest {
         if (ITestResult.FAILURE == result.getStatus()) {
             utils.AllureUtils.takeScreenshot(driver);
         }
-        driver.quit();
+        if (driver !=null) {
+            driver.quit();
+        }
     }
 }
