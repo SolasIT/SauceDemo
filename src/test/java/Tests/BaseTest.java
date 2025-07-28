f5681add5b876c82f8c6bc87f3b7173bd8730ecf
Olga <o.felassan@gmail.com>
2025-07-28 19:43:37 +0300

    fix yaml

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
@@ -45,7 +46,9 @@ public class BaseTest {
             options.addArguments(("--headless"));
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments(("--headless"));
+            driver = new EdgeDriver(options);
         }
         softAssert = new SoftAssert();
 
